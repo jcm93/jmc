90821037da38a8aaa7e50eb745d9b9b643eb9aeb
jcm93 <john.moody@coloradocollege.edu>
2018-02-05 14:37:41 -0700

    move files

---
+++ jmc/Core Data Shit/Album+CoreDataClass.swift
@@ -0,0 +1,27 @@
+//
+//  Album+CoreDataClass.swift
+//  jmc
+//
+//  Created by John Moody on 6/3/17.
+//  Copyright © 2017 John Moody. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Album: NSManagedObject {
+    func getMiscellaneousFiles() -> [NSObject] {
+        var albumFiles = [NSObject]()
+        if self.primary_art != nil {
+            albumFiles.append(self.primary_art!)
+        }
+        if let otherArt = self.other_art?.array {
+            albumFiles.append(contentsOf: otherArt as! [NSObject])
+        }
+        if let otherFiles = self.other_files?.allObjects {
+            albumFiles.append(contentsOf: otherFiles as! [NSObject])
+        }
+        return albumFiles.flatMap({return $0})
+    }
+}
---
+++ jmc/Core Data Shit/AlbumArtwork+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumArtwork+CoreDataClass.swift
+//  jmc
+//
+//  Created by John Moody on 9/20/17.
+//  Copyright © 2017 John Moody. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class AlbumArtwork: NSManagedObject {
+
+}
